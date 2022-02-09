440e968e4f64c12aca71ff5f7189647ffe912ea2
Thiago Monteiro de Paula <thiago.paula@luizalabs.com>
2022-02-09 01:05:54 -0300

    test: LoginBox coverage

--- src/components/__tests__/LoginBox.spec.tsx
+++ src/components/__tests__/LoginBox.spec.tsx
@@ -1,15 +1,17 @@
-import { config } from '../../config/default';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { environment } from '../../config/environment';
+import { render, screen } from '@testing-library/react';
 import { LoginBox } from '../LoginBox';
 import _ from 'lodash';
 
-test('Renders Login button link', () => {
-  render(<LoginBox />);
-  const loginButton = screen.getByText('Entrar com Github');
+describe('Success scenarios', () => {
+  test('SLB001 - Renders Login button link', () => {
+    render(<LoginBox />);
+    const loginButton = screen.getByText('Entrar com Github');
 
-  const githubUrl = _.template(config.github.signInUrl)({
-    clientId: config.github.clientId
+    const githubUrl = _.template(environment.github.signInUrl)({
+      clientId: environment.github.clientId
+    });
+
+    expect(loginButton.getAttribute('href')).toBe(githubUrl);
   });
-
-  expect(loginButton.getAttribute('href')).toBe(githubUrl);
 });
